3b6cd8394336ea1f633ca6286ec7795b5331104f
Kev2Cod <kevinity310@gmail.com>
2022-08-28 20:59:33 +0700

    first commit

---
+++ components/PostCard.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import moment from "moment";
+import Link from "next/link";
+
+const PostCard = ({ post }) => {
+  return (
+    <div className="bg-white shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8">
+      <div>
+        <span>Lanjut: 58:38</span>
+      </div>
+    </div>
+  );
+};
+
+export default PostCard;
